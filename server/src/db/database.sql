6921dd7af25715306f74eca74cba52a9e0559134
vattcarter7 <vattcarter@gmail.com>
2020-03-09 11:27:11 +0700

    fixed indexes

--- server/src/db/database.sql
+++ server/src/db/database.sql
@@ -67,7 +67,7 @@ CREATE TABLE languages(
 CREATE TABLE guides(
   id                INT UNIQUE PRIMARY KEY NOT NULL, -- id DERIVED FROM users id
   nickname          VARCHAR(100),
-  language_id       INT[] REFERENCES languages(id) NOT NULL,
+  language_id       INT REFERENCES languages(id) NOT NULL,
   profile_picture   TEXT NOT NULL,
   date_of_birth     TIMESTAMPTZ NOT NULL,
   about_me          TEXT NOT NULL,
@@ -84,7 +84,22 @@ CREATE TABLE guides(
   active            BOOLEAN DEFAULT true
 );
 
-CREATE INDEX guides_tokens_idx ON tours USING gin(tokens);
+CREATE INDEX guides_tokens_idx ON guides USING gin(tokens);
+
+-- CREATE TABLE customers
+CREATE TABLE customers(
+  id                INT UNIQUE PRIMARY KEY NOT NULL,  -- id DERIVED FROM users id
+  country           VARCHAR(100) NOT NULL,
+  phone             VARCHAR(100)[] NOT NULL,
+  email             VARCHAR(100)[] NOT NULL, 
+  language_id       INT REFERENCES languages(id) NOT NULL,
+  profile_picture   TEXT,
+  tokens            tsvector,
+  created_at        TIMESTAMPTZ DEFAULT now(),
+  active            BOOLEAN DEFAULT true
+);
+
+CREATE INDEX customer_tokens_idx ON customers USING gin(tokens);
 
 -- CREATE TABLE reviews
 CREATE TABLE reviews(
@@ -102,23 +117,9 @@ CREATE TABLE reviews(
 
 CREATE INDEX reviews_customer_id_idx ON reviews(customer_id);
 CREATE INDEX reviews_tour_id_idx ON reviews(tour_id);
-CREATE INDEX reviews_guide_id-idx ON reviews(guide_id);
+CREATE INDEX reviews_guide_id_idx ON reviews(guide_id);
 CREATE INDEX reviews_tokens_idx ON reviews USING gin(tokens);
 
--- CREATE TABLE customers
-CREATE TABLE customers(
-  id                INT UNIQUE PRIMARY KEY NOT NULL,  -- id DERIVED FROM users id
-  country           VARCHAR(100) NOT NULL,
-  phone             VARCHAR(100)[] NOT NULL,
-  email             VARCHAR(100)[] NOT NULL, 
-  language_id       INT REFERENCES languages(id) NOT NULL,
-  profile_picture   TEXT,
-  tokens            tsvector,
-  created_at        TIMESTAMPTZ DEFAULT now(),
-  active            BOOLEAN DEFAULT true
-);
-
-CREATE INDEX customer_tokens_idx ON customers USING gin(tokens);
 
 -- CREATE TABLE purchases
 CREATE TABLE purchases(
@@ -166,5 +167,5 @@ CREATE TABLE guide_request_details(
   created_at            TIMESTAMPTZ DEFAULT now()
 );
 
-CREATE INDEX guide-request_details_guide_request_id_idx ON guide_request_details(guide_request_id);
-CREATE INDEX guide-request_details_guide_id_idx ON guide_request_details(guide_id);
+CREATE INDEX guide_request_details_guide_request_id_idx ON guide_request_details(guide_request_id);
+CREATE INDEX guide_request_details_guide_id_idx ON guide_request_details(guide_id);
