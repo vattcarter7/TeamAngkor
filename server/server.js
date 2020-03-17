9748be57f5a0531638c677887f667dfead67a693
vattcarter7 <vattcarter@gmail.com>
2020-03-17 14:18:58 +0700

    modified directories to work with client

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
   if (err) throw err;
