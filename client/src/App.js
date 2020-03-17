88e4b47fb3d0b70495fe5449592ae85f6ef6299f
vattcarter7 <vattcarter@gmail.com>
2020-03-17 15:31:39 +0700

    implement redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+// Redux
+import {Provider} from 'redux'
+import store from './store';
+
+
+
 import "./App.css";
 
 const App = () => {
   return (
     <div className='App'>
-        <h3>Hello World</h3>
+      <h3>Hello World</h3>
     </div>
   );
 };
